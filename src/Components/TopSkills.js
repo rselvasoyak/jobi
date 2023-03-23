9b64c65767dbfdb38c344deeff66c95249b0e55e
Rana Soyak <rselvasoyak@gmail.com>
2023-03-22 21:50:38 -0400

    basic html and some css

---
+++ src/Components/TopSkills.js
@@ -0,0 +1,58 @@
+const TopSkills = () => {
+    return(
+        <section className="bottomFooter">
+            <div className="wrapper">
+                <nav>
+                    <div className="navColumn">
+                        <h4 className="mainSkill"> trending skills </h4>
+                        <ul>
+                            <li>blockchain</li>
+                            <li>node.js</li>
+                            <li>HR consulting</li>
+                            <li>vue.js</li>
+                            <li>microsoft power bI</li>
+                            <li>react.js</li>
+                            <li>videographers</li>
+                        </ul>
+                    </div>
+                    <div className="navColumn">
+                        <h4> top skills </h4>
+                        <ul>
+                            <li>full consultation</li>
+                            <li>code review</li>
+                            <li>staff augmentation</li>
+                            <li>support</li>
+                            <li>video editors</li>
+                            <li>data entry specialist</li>
+                            <li>data analyst</li>
+                        </ul>
+                    </div>
+                    <div className="navColumn">
+                        <h4> Top in USA </h4>
+                        <ul>
+                            <li>technical support</li>
+                            <li>accountants</li>
+                            <li>web designers in US</li>
+                            <li>customer identity</li>
+                            <li>data entry</li>
+                        </ul>
+                    </div>
+                    <div className="navColumn">
+                        <h4> project catalog </h4>
+                        <ul>
+                            <li>microsites</li>
+                            <li>marketing automation</li>
+                            <li>SEO & SMM</li>
+                            <li>lead gemeration</li>
+                            <li>article writing services</li>
+                            <li>SEO services</li>
+                            <li>translation services</li>
+                        </ul>
+                    </div>
+                </nav>
+            </div> {/* g */}
+        </section>
+    )
+}
+
+export default TopSkills;
