95de9a6dabb1ce9491dc5dec8d5809de5e94d433
Rana Soyak <rselvasoyak@gmail.com>
2023-03-23 23:50:06 -0400

    trending skills style fix

--- src/Components/TrendingServices.js
+++ src/Components/TrendingServices.js
@@ -8,39 +8,47 @@ const TrendingServices = () => {
             <div className="wrapper">
                 <div className="serviceTopRow">
                     <h2> trending services </h2>
-                    <div className="carouselButtons">
+                    {/* <div className="carouselButtons">
                         <button className="left"> 
                             <img src={ left }alt="left button arrow"></img>
                         </button>
                         <button className="right"> 
                             <img src={ right }alt="right button arrow"></img>
                         </button>
-                    </div>
+                    </div> */}
                 </div>
                 <ul className="serviceLinks">
                     <li className="service">
                         <div className="serviceImgCont">
                             {/* <img></img> */}
                         </div>
-                        <p><span> wordPress </span> development.</p>
+                        <div className="serviceTitle">
+                            <p><span> wordPress </span> development.</p>
+                        </div>
                     </li>
                     <li className="service">
                         <div className="serviceImgCont">
                             {/* <img></img> */}
                         </div>
-                        <p><span> audio & </span> video editing.</p>
+                        <div className="serviceTitle">
+                           <p><span> audio & </span> video editing.</p>
+                        </div>
                     </li>
                     <li className="service">
                         <div className="serviceImgCont">
                             {/* <img></img> */}
                         </div>
-                        <p><span> product & </span> branding design</p>
+                        <div className="serviceTitle">
+                            <p><span> product & </span> branding design</p>
+                        </div>
                     </li>
                     <li className="service">
                         <div className="serviceImgCont">
                             {/* <img></img> */}
                         </div>
-                        <p> <span> admin & </span> customer support </p>
+                        <div className="serviceTitle">
+                            <p><span> admin & </span> customer support </p>
+                        </div>
                     </li>
                 </ul> {/* serviceLinks ending */}
 
