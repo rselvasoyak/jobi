780b5310df34e5395aa9874825c3c1f6cb026a13
Rana Soyak <rselvasoyak@gmail.com>
2023-03-29 13:11:19 -0400

    job lists page and main firebase connection done

--- src/Components/JobLists.js
+++ src/Components/JobLists.js
@@ -1,6 +1,47 @@
-const JobLists = () => {
+import google from "../partials/assets/mvpAssets/Logo-1.png"
+
+const JobLists = ({jobs}) => {
     return(
-        <p>Job List </p>
+        <section className="jobLists"> 
+            <div className="topTopRow"> 
+                <p>
+                   Top Row 
+                </p>
+            </div>
+            <div className="listsContainer">
+                {jobs.map ((job)=> {
+                    // console.log(job); 
+                    const { key, contractType, title} = job;
+                    return(
+                        <div className="job" key={key}>
+                            <div className="topListRow">
+                                <div className="logoCont">
+                                    <img src={google} alt=""></img>
+                                </div>
+                                <div>
+                                    <button>
+                                        Bookmark
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="listContent">
+                                <div>
+                                    <p className="jobType">{contractType}</p>
+                                    <p className="jobTitle"> {title}</p>
+                                    <p className="salary"> $300-$450 <span className="freq"> /Week</span></p>
+                                </div>
+                            </div>
+                            <div className="bottomListRow">
+                                <p>USA, California</p>
+                                <div>
+                                    <a href=" " className="apply"> apply</a>
+                                </div>
+                            </div> 
+                        </div> 
+                    );
+                })}
+            </div>  
+    </section>
     )
 }
 
