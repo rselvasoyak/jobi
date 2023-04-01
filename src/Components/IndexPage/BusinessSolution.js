abf6a5df5085f4e684d70157df6c9b753d8f593a
Rana Soyak <rselvasoyak@gmail.com>
2023-04-01 18:18:34 -0400

    about us section accessibility and style fix

--- src/Components/IndexPage/BusinessSolution.js
+++ src/Components/IndexPage/BusinessSolution.js
@@ -6,22 +6,22 @@ const BusinessSolution = () => {
                     <div className="businessText">
                         <div className="topContent">
                             <h4> business solution </h4>
-                            <h2> Get quick <span>Solution for your</span> <span className="emphasize">business</span></h2>
+                            <h2> Get quick <span>Solution For Your</span> <span className="emphasize">Business</span></h2>
                             <p>
                                A full suite of hybrid workforce management tools are yours to use, as well as access to our top 1% of talent. 
                             </p>
                         </div>
                         <div className="bottomInfo">
                             <div className="stats">
-                                <div className="stat">
+                                <div className="stat" aria-label="Worldwide Clients">
                                     <p className="statTitle"> 30 k+</p>
                                     <p>Worldwide Client</p>
                                 </div>
-                                <div className="stat">
+                                <div className="stat" aria-label="Top Talents">
                                     <p className="statTitle"> 3% </p>
                                     <p>Top Talents</p>
                                 </div>
-                                <div className="stat">
+                                <div className="stat" aria-label="Dollar Payout">
                                     <p className="statTitle"> 7mil</p>
                                     <p>Doller Payout</p>
                                 </div>
@@ -29,10 +29,10 @@ const BusinessSolution = () => {
                         </div>{/* bottom nav ending */}
                     </div>{/* business content ending */}
                     <div>
-                        <a className="secondaryLink exploreJobi" href=" "> explore jobi business</a>
+                        <a className="exploreJobi" href=" "> explore jobi business</a>
                     </div>
                 </div>{/* business content ending */}
-                <div className="businessImg">
+                <div className="businessImg" aria-hidden="true">
 
                 </div>
             </div>{/* wrapper ending */}
