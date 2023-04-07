aa81e15963aa458a923dcf13db65d0a9dc88e07e
Rana Soyak <rselvasoyak@gmail.com>
2023-04-07 15:27:08 -0400

    w.i.p- simple routing to single job page

--- src/Components/JobListsPage/JobLists.js
+++ src/Components/JobListsPage/JobLists.js
@@ -1,5 +1,6 @@
 import google from "../../partials/assets/mvpAssets/Logo-1.png"
 import bookmark from "../../partials/assets/mvpAssets/Bookmark-1.png"
+import {Link} from "react-router-dom";
 
 const JobLists = ({jobs}) => {
     return(
@@ -14,31 +15,33 @@ const JobLists = ({jobs}) => {
                     // console.log(job); 
                     const { key, contractType, title, location} = job;
                     return(
-                        <div className="job" key={key}>
-                            <div className="topListRow">
-                                <div className="logoCont" aria-label="Google logo">
-                                    <img src={google} alt="google logo"></img>
-                                </div>
-                                <div>
-                                    <button aria-label="Bookmark this job">
-                                        <img src={bookmark} alt="bookmarking icon" aria-label="Bookmark your job"></img>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="listContent">
-                                <div>
-                                    <p className="jobType">{contractType}</p>
-                                    <p className="jobTitle"> {title}</p>
-                                    <p className="salary"> $300-$450 <span className="freq"> /Week</span></p>
-                                </div>
-                            </div>
-                            <div className="bottomListRow">
-                                <p>{location.country},{location.city}</p>
-                                <div>
-                                    <a href="/" className="apply" aria-label="Apply for this job"> apply</a>
-                                </div>
-                            </div> {/* bottom list ending */}
-                        </div> /* job ending */
+                        <Link to="/singleJobPage" className="job" key={key}>
+                                {/* <div className="job" key={key}> */}
+                                    <div className="topListRow">
+                                        <div className="logoCont" aria-label="Google logo">
+                                            <img src={google} alt="google logo"></img>
+                                        </div>
+                                        <div>
+                                            <button aria-label="Bookmark this job">
+                                                <img src={bookmark} alt="bookmarking icon" aria-label="Bookmark your job"></img>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="listContent">
+                                        <div>
+                                            <p className="jobType">{contractType}</p>
+                                            <p className="jobTitle"> {title}</p>
+                                            <p className="salary"> $300-$450 <span className="freq"> /Week</span></p>
+                                        </div>
+                                    </div>
+                                    <div className="bottomListRow">
+                                        <p>{location.country},{location.city}</p>
+                                        <div>
+                                            <a href="/" className="apply" aria-label="Apply for this job"> apply</a>
+                                        </div>
+                                    </div> {/* bottom list ending */}
+                                {/* </div> {/* /* job ending */ } 
+                        </Link>
                     );
                 })}
             </div>  {/* list container ending */}
