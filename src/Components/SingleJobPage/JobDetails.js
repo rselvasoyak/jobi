076603d71be47d968b3ef3cabf04a5e54d9260ef
Rana Soyak <rselvasoyak@gmail.com>
2023-04-06 21:25:24 -0400

    work in progress - main css layout

--- src/Components/SingleJobPage/JobDetails.js
+++ src/Components/SingleJobPage/JobDetails.js
@@ -1,59 +1,65 @@
 const JobDetails = () => {
     return(
-        <section>
-            <div className="singleJobTitle">
-                <p> 18 Jul 2022 by <span>Adobe</span></p>
-                <h3>Senior Product & Brand Design</h3>
-                <div className="companyLinks">
-                    <div className="media">
-                        <a href="https://www.facebook.com/"> Facebook </a>
+        <section  className="jobDetails" aria-label="Job Details">
+            <div className="wrapper">
+                <div className="singleJobTitle">
+                    <p> 18 Jul 2022 by <span>Adobe</span></p>
+                    <h3>Senior Product & Brand Design</h3>
+                    <div className="companyLinks">
+                        <div className=" link media">
+                            <a href="https://www.facebook.com/"> Facebook </a>
+                        </div>
+                        <div className=" link media">
+                            <a href="https://www.facebook.com/"> Twitter </a>
+                        </div>
+                        <button className="link copy">
+                            Copy
+                        </button>
                     </div>
-                    <div className="media">
-                        <a href="https://www.facebook.com/"> Facebook </a>
-                    </div>
-                    <div className="media">
-                        <a href="https://www.facebook.com/"> Twitter </a>
-                    </div>
-                    <button className="copy">
-                        Copy
-                    </button>
                 </div>
-            </div>
-            <div className="overview">
-                <h4>Overview:</h4>
-                <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga, excepturi atque! Deserunt laborum velit aspernatur doloremque odio. Eius culpa sint enim inventore aut eveniet eos soluta quaerat magnam? Non consequatur molestias repellat minus ut? Fugiat.<a href="/">Learn more here</a> Please indicate which location(s) you are interested. </p>
-            </div>
-            <div className="jobDescription">
-                <h4>Job Description:</h4>
-                <p> as a <span>Product Designer</span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga, excepturi atque! Deserunt laborum velit aspernatur doloremque odio.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga, excepturi atque! Deserunt laborum velit aspernatur doloremque odio. Eius culpa sint enim inventore aut eveniet eos soluta quaerat magnam? Non consequatur molestias repellat minus ut? Fugiat.</p>
-            </div>
-            <div className="responsabilities">
-                <h4>Responsabilities:</h4>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-            </div>
-            <div className="requiredSkills">
-                <h4>Required Skills:</h4>
-                <p className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-            </div>
-            <div className="benefits">
-                <h4>Benefits:</h4>
-                <p className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
-                <p className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</p>
+                <div className="detail overview">
+                    <h4>Overview:</h4>
+                    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga, excepturi atque! Deserunt laborum velit aspernatur doloremque odio. Eius culpa sint enim inventore aut eveniet eos soluta quaerat magnam? Non consequatur molestias repellat minus ut? Fugiat.<span><a href="/">Learn more here</a></span> Please indicate which location(s) you are interested. </p>
+                </div>
+                <div className=" detail jobDescription">
+                    <h4>Job Description:</h4>
+                    <p> As a <span>Product Designer</span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga, excepturi atque! Deserunt laborum velit aspernatur doloremque odio.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga, excepturi atque! Deserunt laborum velit aspernatur doloremque odio. Eius culpa sint enim inventore aut eveniet eos soluta quaerat magnam? Non consequatur molestias repellat minus ut? Fugiat.</p>
+                </div>
+                <div className=" detail responsabilities">
+                    <h4>Responsabilities:</h4>
+                    <ul>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="singleResp">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    </ul>
+                </div>
+                <div className=" detail requiredSkills">
+                    <h4>Required Skills:</h4>
+                    <ul>
+                    <li className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    <li className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    <li className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    <li className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    <li className="skill">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    </ul>
+                </div>
+                <div className=" detail benefits">
+                    <h4>Benefits:</h4>
+                    <ul>
+                        <li className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                        <li className="benefit">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque deserunt, culpa tempore itaque asperiores nisi ex cum velit exercitationem alias magni vero reiciendis fuga</li>
+                    </ul>
+
+                </div>
             </div>
         </section>
     )
