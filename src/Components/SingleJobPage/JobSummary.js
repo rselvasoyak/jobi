d66ba52d5a91ae39b1146164e74478d06321fe5b
Rana Soyak <rselvasoyak@gmail.com>
2023-04-07 14:53:27 -0400

    main markup and styling done 🌟

--- src/Components/SingleJobPage/JobSummary.js
+++ src/Components/SingleJobPage/JobSummary.js
@@ -7,7 +7,10 @@ const JobSummary = () => {
                     <div className="companyImg">
                         <img src="" alt="Company Logo"></img>
                     </div>
-                    <a className="secondaryLink" href="/" title="Visit Company Website"> Visit Website </a>
+                    <p className="companyName">Company</p>
+                    <div>
+                        <a className="secondaryLink" href="/" title="Visit Company Website"> Visit Website </a>
+                    </div>
                 </div>
                 <div className="mainDetails">
                     <div className="jobMainInfo">
@@ -25,7 +28,7 @@ const JobSummary = () => {
                                 <p className="jobSpecs">test</p>
                             </div>
                         </div>
-                        <div className="mainInfo">
+                        <div className="mainInfo second">
                             <div className="info">
                                 <p className="heading">Salary</p>
                                 <p className="jobSpecs">50k</p>
@@ -35,8 +38,8 @@ const JobSummary = () => {
                                 <p className="jobSpecs"> Barcelna</p>
                             </div>
                             <div className="info">
-                                <p className="heading"> test</p>
-                                <p className="jobSpecs">test</p>
+                                <p className="heading"> Experience</p>
+                                <p className="jobSpecs">2 years</p>
                             </div>
                         </div>
                     </div>
@@ -44,12 +47,12 @@ const JobSummary = () => {
                         <ul>
                             <li> design</li>
                             <li> product design</li>
+                            <li> ui ux</li>
                             <li> brands</li>
                             <li> application</li>
-                            <li> ui ux</li>
                         </ul>
                     </div>
-                    <a href="/" className="primaryLink">Apply Now</a>
+                    <a href="/" className="primaryLink applyLink">Apply Now</a>
                 </div>{/* main details ending */}
            </div>{/* wrapper ending */}
         </aside>
